88d80d26e978090b585fa844e975e3a3949b4455
JOSE IGNACIO VARGAS JARAMILLO <135558719+Getz7@users.noreply.github.com>
2023-07-13 20:42:23 -0600

    objetos 3

--- Backend/src/casoestudio/objetos/Proforma.java
+++ Backend/src/casoestudio/objetos/Proforma.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 
 public class Proforma {
     private ArrayList<Repuesto> lineasProformas;
+    private Vendedor vendedor;
+    private String codigo;
+    private String estado;
 
-    public Proforma(ArrayList<Repuesto> lineasProformas) {
+    public Proforma(ArrayList<Repuesto> lineasProformas, Vendedor vendedor, String codigo, String estado) {
         this.lineasProformas = lineasProformas;
+        this.vendedor = vendedor;
+        this.codigo = codigo;
+        this.estado = estado;
     }
 
     public ArrayList<Repuesto> getLineasProformas() {
@@ -17,10 +23,37 @@ public class Proforma {
         this.lineasProformas = lineasProformas;
     }
 
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
     @Override
     public String toString() {
         return "Proforma{" +
                 "lineasProformas=" + lineasProformas +
+                ", vendedor=" + vendedor +
+                ", codigo='" + codigo + '\'' +
+                ", estado='" + estado + '\'' +
                 '}';
     }
 }
